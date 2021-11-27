8d0d485e745543c05580a95b96c1f255e10a767d
Satyamall <thesatyamall123@gmail.com>
2021-11-27 14:07:39 +0530

    Satya, React useref code in react of we

---
+++ src/App.js
@@ -0,0 +1,100 @@
+
+import './App.css';
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+  const [time, setTime] = useState(0);
+
+  const timeRef = useRef(null);
+
+  useEffect(() => {
+    return handlePause;
+  }, [])
+
+  const handleStart = () => {
+    if (timeRef.current === null) {
+      timeRef.current = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000)
+    }
+  }
+  const handlePause = () => {
+    clearInterval(timeRef.current);
+    timeRef.current = null;
+  }
+  const handleReset = () => {
+    handlePause();
+    setTime(0)
+  }
+
+  const ref = useRef(null);
+
+  const handleMoveToTopBottom = () => {
+    if (ref.current) {
+      ref.current.scrollTop = 0;
+    }
+  };
+
+  const inputRef=useRef(null);
+
+  const handleChange=()=>{
+    inputRef.current.focus();
+  }
+  console.log(inputRef)
+
+  return (
+    <div className="App">
+      <h1>1. Timer Application</h1>
+      <h1>{time}</h1>
+      <div>
+        <button onClick={handleStart}>START</button>
+        <button onClick={handlePause}>PAUSE</button>
+        <button onClick={handleReset}>RESET</button>
+      </div>
+
+      <h1>2. Container</h1>
+      <div>
+        <button onClick={handleMoveToTopBottom}>Scroll To Top and Bottom</button>
+        <div
+          ref={ref}
+          style={{
+            maxHeight: 200,
+            overflow: "hidden",
+            overflowY: "auto",
+            border: "1px solid black",
+            padding: 20,
+            width: 200,
+            background: "white",
+            color: "black"
+          }}
+        >
+          <div
+            style={{
+              height: 1024,
+              border: "1px solid gray"
+            }}
+          >
+            create a container with overflow y to auto and give a max height.
+            Inside element should have a height more than parent.
+            you can fill with dummy data inside.
+            create a button to scroll to the top and bottom.
+            create a container with overflow y to auto and give a max height.
+            Inside element should have a height more than parent.
+            you can fill with dummy data inside.
+            create a button to scroll to the top and bottom.
+          </div>
+        </div>
+      </div>
+       
+       <h1>3. Simple Form to Upload an Image</h1>
+      <div>
+        <label>Picture File: </label>
+        <input ref={inputRef} type="file" onChange={handleChange}/>
+        <br/>
+        <img src={inputRef.current} alt=""/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
